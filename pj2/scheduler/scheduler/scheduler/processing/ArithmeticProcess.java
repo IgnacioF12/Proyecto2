fbbe1392a5a7067523b8f34df8953e99fc4dfd25
ErickMotta20 <158476316+ErickMotta20@users.noreply.github.com>
2024-11-14 17:48:19 -0600

    Creación del proceso ArithmeticProcess

--- pj2/scheduler/scheduler/scheduler/processing/ArithmeticProcess.java
+++ pj2/scheduler/scheduler/scheduler/processing/ArithmeticProcess.java
@@ -4,11 +4,22 @@
 ** Carnet: 24003932
 ** Seccion: A
 **/
+//*Implementación del proceso ArithmeticProcess, que permite que daca proceso aritmético tenga un tiempo de ejecución fijo*//
 
 package scheduler.processing;
+//*Tiempo fijo en milisegundos para los procesos aritméticos*//
+public class ArithmeticProcess extends SimpleProcess {
 
-public abstract class ArithmeticProcess extends SimpleProcess {
-    public ArithmeticProcess(Integer placeholder) {
-        super(placeholder);
+    private static final long TIEMPO_PROCESO = 400; 
+
+    //*Constructor pra el proceso*//
+    public ArithmeticProcess(int id) {
+        super(id, TIEMPO_PROCESO); //*Se le asigna su id y tiempo fijo al proceso*//
+    }
+
+//*Se ejecuta el proceso, disminyuendo el tiempo restante según el tiempo ingresado*//
+    @Override
+    public void ejecutar(long tiempo) {
+        setTiempoRestante(getTiempoRestante() - tiempo);
     }
 }
