8a8004306f4e93a273f2933addac5a2466dcb498
IgnacioF <ignacio.flores@galileo.edu>
2024-11-14 16:22:41 -0600

    Conditional process

--- pj2/scheduler/scheduler/scheduler/processing/ConditionalProcess.java
+++ pj2/scheduler/scheduler/scheduler/processing/ConditionalProcess.java
@@ -1,9 +1,30 @@
-/* ConditionalProcess.java */
 
+/* ConditionalProcess.java */
 package scheduler.processing;
 
-public abstract class ConditionalProcess extends SimpleProcess {
-    public ConditionalProcess(Integer placeholder) {
-        super(placeholder);
+import scheduler.processing.SimpleProcess;
+
+public class ConditionalProcess extends SimpleProcess {
+    private long t;
+
+    public ConditionalProcess(int id, long t) {
+        super(id);
+        this.t = t;
+    }
+
+    public long getT() {
+        return t;
+    }
+
+    public void run(long t) {
+        try {
+            if (t < 0) {
+                throw new Error("Tiempo de servicio invalido");
+            }
+            System.out.println("Ejecutando proceso " + getId() + " con tiempo de " + t);
+            Thread.sleep(t);
+        } catch (Exception err) {
+            throw new Error(err);
+        }
     }
 }
