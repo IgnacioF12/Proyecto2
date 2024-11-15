97674f1b7bfdf477246e472a2ac5d0d2dc61a415
IgnacioF <ignacio.flores@galileo.edu>
2024-11-15 15:57:49 -0600

    Cambios minimos

--- pj2/scheduler/scheduler/scheduler/processing/ConditionalProcess.java
+++ pj2/scheduler/scheduler/scheduler/processing/ConditionalProcess.java
@@ -8,20 +8,22 @@ public class ConditionalProcess extends SimpleProcess {
     private long t;
 
     public ConditionalProcess(int id, long t) {
-        super(id);
+        super(id, t);
         this.t = t;
     }
 
-    public long getT() {
+    @Override
+    public long getTiempoRestante() {
         return t;
     }
 
-    public void run(long t) {
+    @Override
+    public void ejecutar(long t) {
         try {
             if (t < 0) {
                 throw new Error("Tiempo de servicio invalido");
             }
-            System.out.println("Ejecutando proceso " + getId() + " con tiempo de " + t);
+            System.out.println("Ejecutando proceso " + getId() + " con tiempo de " + t + "ms");
             Thread.sleep(t);
         } catch (Exception err) {
             throw new Error(err);
