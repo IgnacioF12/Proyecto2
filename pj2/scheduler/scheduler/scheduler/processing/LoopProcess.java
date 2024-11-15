71b50eff35c225e534464af4cd02f8f97bca6d0c
ErickMotta20 <158476316+ErickMotta20@users.noreply.github.com>
2024-11-15 15:54:17 -0600

    creación de LoopProcess

--- pj2/scheduler/scheduler/scheduler/processing/LoopProcess.java
+++ pj2/scheduler/scheduler/scheduler/processing/LoopProcess.java
@@ -1,9 +1,25 @@
 /* LoopProcess.java */
+/**
+** Hecho por: Erick Motta
+** Carnet: 24003932
+** Seccion: A
+**/
+//*Implementación del proceso LoopProcess, que permite un proceso iterativo que simula tareas repetitivas*//
 
 package scheduler.processing;
 
-public abstract class LoopProcess extends SimpleProcess {
-    public LoopProcess(Integer placeholder) {
-        super(placeholder);
+public class LoopProcess extends SimpleProcess {
+//*Tiempo fijo en milisegundos para los procesos de tipo loop*//
+    private static final long TIEMPO_PROCESO = 600; 
+
+    //*Constructor pra el proceso*//
+    public LoopProcess(int id) {
+        super(id, TIEMPO_PROCESO); //*Se le asigna su id y tiempo fijo al proceso*//
+    }
+
+    //*Se ejecuta el proceso, disminyuendo el tiempo restante según el tiempo ingresado*//
+    @Override
+    public void ejecutar(long tiempo) {
+        setTiempoRestante(getTiempoRestante() - tiempo);
     }
 }
