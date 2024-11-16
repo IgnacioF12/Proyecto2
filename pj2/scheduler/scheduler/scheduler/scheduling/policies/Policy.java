8a454228d8e362acce51d3f4d9b90626e85099d0
IgnacioF <ignacio.flores@galileo.edu>
2024-11-15 18:09:12 -0600

    Ignorar

--- pj2/scheduler/scheduler/scheduler/scheduling/policies/Policy.java
+++ pj2/scheduler/scheduler/scheduler/scheduling/policies/Policy.java
@@ -6,39 +6,48 @@
 
 	@author Ing. Andrea Quan
 **/
-	
+
 package scheduler.scheduling.policies;
 
 import scheduler.processing.SimpleProcess;
-	
- public abstract class Policy implements Enqueable {
-	/** El campo size va a representar el numero de procesos activos, 
-	es decir, en la cola actualmente, en este momento 
-        **/
-	protected int size;
-	/** El campo total va a representar el total de procesos que han sido ingresados a
-        la cola **/
-	protected int totalProcesses;
+
+public abstract class Policy implements Enqueable {
 	/**
-		Inicializa el Policy con size = 0 y totalProcesses = 0.
-	**/		
+	 * El campo size va a representar el numero de procesos activos,
+	 * es decir, en la cola actualmente, en este momento
+	 **/
+	protected int size;
+	/**
+	 * El campo total va a representar el total de procesos que han sido ingresados
+	 * a
+	 * la cola
+	 **/
+	protected int totalProcesses;
+
+	/**
+	 * Inicializa el Policy con size = 0 y totalProcesses = 0.
+	 **/
 	public Policy() {
 		this.size = 0;
 		this.totalProcesses = 0;
 	}
-        /**
-		Devuelve la cantidad de procesos activos en la cola de la politica
-		@return devuelve un entero que representa la cantidad de procesos
-                en la cola actualmente
-	**/	
+
+	/**
+	 * Devuelve la cantidad de procesos activos en la cola de la politica
+	 * 
+	 * @return devuelve un entero que representa la cantidad de procesos
+	 *         en la cola actualmente
+	 **/
 	public final int size() {
 		return size;
 	}
-        /**
-		Devuelve el total de procesos ingresados a la cola de la politica
-		@return devuelve el entero que representa la cantidad total
-                de procesos ingresados a la cola de la politica
-	**/	
+
+	/**
+	 * Devuelve el total de procesos ingresados a la cola de la politica
+	 * 
+	 * @return devuelve el entero que representa la cantidad total
+	 *         de procesos ingresados a la cola de la politica
+	 **/
 	public final int totalProcesses() {
 		return totalProcesses;
 	}
